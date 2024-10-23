e08bd1accb81bc223f33a6d3ee7c04fab0a058f0
Nidhi <nidhidang495@gmail.com>
2024-10-23 12:27:48 +0530

    MainFile

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+public class Main {
+
+    public static void main(String[] args) {
+        PalindromeNumber p = new PalindromeNumber();
+        p.isPalindrome(234);
+        p.isPalindrome(121);
+    }
+}
+
